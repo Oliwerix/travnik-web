4b68cbb423944cc2053b4c4f3902203a82a750cf
Oliwerix <oliwerix@gmail.com>
2023-02-24 15:59:17 +0100

    http image alt updated

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,12 @@
 <script>
+// @ts-nocheck
+// no check because message will always be not null if error page is shown
     import {page} from '$app/stores'
 </script>
 <div class="center">
     <h1>{$page.status} {$page.error.message}</h1>
     <figure>
-        <img src="https://http.cat/{$page.status}" alt="funny cat image making fun of http code {$page.status}" name="catimage">
+        <img src="https://http.cat/{$page.status}" alt="funny cat image making fun of http code {$page.status}">
         <figcaption>Image courtesy of <a href="https://http.cat/">https://http.cat/</a></figcaption>
     </figure>
 </div>
