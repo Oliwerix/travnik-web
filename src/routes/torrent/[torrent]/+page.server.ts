1525b4708144250d432cd7dc4aa747f97d5940b7
Oliwerix <oliwerix@gmail.com>
2023-02-24 15:59:01 +0100

    throw 404 if torrent not found

--- src/routes/torrent/[torrent]/+page.server.ts
+++ src/routes/torrent/[torrent]/+page.server.ts
@@ -1,8 +1,14 @@
 import { torrenti } from "$db/torrenti";
 import type { PageServerLoad } from "./$types";
+import {error} from '@sveltejs/kit'
 
 export const load: PageServerLoad = async function(page) {
     const data = await torrenti.findOne({infoHash: page.params.torrent}, {projection: {name:1, files: 1,_id: 0, infoHash: 1, created: 1, length: 1, source: 1}});
+    if(data === null) {
+        throw error(404, {
+            message: 'Torrent not found'
+        })
+    }
     return {
         torrent: data
     }    
