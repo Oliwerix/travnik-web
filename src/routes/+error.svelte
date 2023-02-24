41c3a2c9a2537725a40170f4b75007d219cd49c3
Oliwerix <oliwerix@gmail.com>
2023-02-24 14:47:46 +0100

    custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {page} from '$app/stores'
+</script>
+<div class="center">
+    <h1>{$page.status} {$page.error.message}</h1>
+    <figure>
+        <img src="https://http.cat/{$page.status}" alt="funny cat image making fun of http code {$page.status}" name="catimage">
+        <figcaption>Image courtesy of <a href="https://http.cat/">https://http.cat/</a></figcaption>
+    </figure>
+</div>
+
+<style>
+    div.center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    align-items: center;
+}
+</style>
