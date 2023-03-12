a1f70730d0406d6fdace9823cd9280e1af0acef2
Oliwerix <oliwerix@gmail.com>
2023-03-12 19:32:48 +0100

    remove padding files part 1

--- src/routes/torrent/[torrent]/ListView.svelte
+++ src/routes/torrent/[torrent]/ListView.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-    export let files:Array<{path: string, name:string, length:number, offset: number}>
+    type Filelist = Array<{path: string, name:string, length:number, offset: number}>
+    export let files:Filelist
+    function clean_files(files:Filelist): Filelist{
+        for(let i = 0; i < files.length; i++) {
+            // this looks illegal, if I remove an element from an array, do I skip a file?
+            if(files[i].path.split("/").includes(".pad"))
+                files.splice(i, 1)
+            if(files[i].path.includes("padding.file"))
+                files.splice(i, 1)
+            if(files[i].path.includes("_____padding_file_"))
+                files.splice(i, 1)
+        }
+        return files
+    }
 </script>
 
 <ul>
-    {#each files as file}
+    {#each clean_files(files) as file}
         <li>{file.path}</li>
     {/each}
 </ul>
